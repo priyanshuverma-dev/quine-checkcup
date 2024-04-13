bd75b200000eed68c6c3c42b47eb4b1a4a5d4080
priyanshuverma-dev <priyanshu.creator@hotmail.com>
2024-04-13 16:44:50 +0530

    Update browserWSEndpoint to use BROWSERLESS_API_KEY environment variable in site/route.ts

--- app/api/site/route.ts
+++ app/api/site/route.ts
@@ -2,10 +2,11 @@ import { Status } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import puppeteer from "puppeteer";
 
+console.log(process.env.IS_BROWSERLESS_ON);
 const IS_BROWSERLESS = process.env.IS_BROWSERLESS_ON;
 const browserWSEndpoint = `https://production-sfo.browserless.io?token=${process.env.BROWSERLESS_API_KEY}`;
 const getBrowser = async () =>
-  IS_BROWSERLESS
+  IS_BROWSERLESS === "true"
     ? puppeteer.connect({ browserWSEndpoint })
     : puppeteer.launch({
         headless: true,
